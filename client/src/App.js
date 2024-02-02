98093fb11ac6af96238b34b9c1518cedf4cb23ff
Igorlozko <g00390368@atu.ie>
2024-02-02 21:22:49 +0000

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Loading from './components/Loading';
+import NavBar from './components/NavBar';
+import Notification from './components/Notification';
+import Login from './components/user/Login';
+import BottomNav from './components/BottomNav';
+
+const App = () => {
+  return (
+    <>
+      <Loading />
+      <Notification />
+      <Login />
+      <NavBar />
+      <BottomNav/>
+    </>
+  );
+};
+
+export default App;
